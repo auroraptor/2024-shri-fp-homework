04be6200a4d0716da5cc930b061d395daf9be6d9
auroraptor <interestingbutno@yandex.ru>
2024-07-28 15:37:11 +0300

    fix: update validation logic for three non-white shapes of the same color

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,44 +1,50 @@
 import {
   compose,
   allPass,
-  anyPass,
   __,
   values,
-  reduce,
+  sort,
   equals,
   prop,
   filter,
   length,
   gte,
   all,
+  converge,
+  not,
+  map,
+  T,
 } from "ramda";
+
+import { SHAPES, COLORS } from "../constants";
+
+// -2. Деструктуризация констант
+const { TRIANGLE, SQUARE, CIRCLE, STAR } = SHAPES;
+const { RED, BLUE, ORANGE, GREEN, WHITE } = COLORS;
 
 // -1. Вспомогательные функции
 const isColor = (color) => equals(color);
 const getColor = (shape) => prop(shape);
 const isShapeColor = (shape, color) => compose(isColor(color), getColor(shape));
-const isRed = isColor("red");
-const isGreen = isColor("green");
-const isWhite = isColor("white");
-const isBlue = isColor("blue");
 const shapesToArray = ({ star, square, triangle, circle }) => [
   star,
   square,
   triangle,
   circle,
 ];
-const countNonWhiteColors = compose(
-  reduce((acc, color) => ({ ...acc, [color]: (acc[color] || 0) + 1 }), {}),
-  filter((color) => color !== "white")
-);
+const countShapesByColor = (color) => compose(length, filter(isColor(color)));
+const shapesWithoutWhite = filter(compose(not, isColor(WHITE)));
+const sortColors = sort((a, b) => a.localeCompare(b));
+const hasAtLeastThreeSameColor = (sorted) =>
+  (sorted[0] === sorted[2] || sorted[1] === sorted[3]) && gte(sorted.length, 3);
 
 // 0. Проверки для каждого цвета
-const isRedStar = isShapeColor("star", "red");
-const isGreenSquare = isShapeColor("square", "green");
-const isWhiteTriangle = isShapeColor("triangle", "white");
-const isWhiteCircle = isShapeColor("circle", "white");
-const isBlueCircle = isShapeColor("circle", "blue");
-const isOrangeSquare = isShapeColor("square", "orange");
+const isRedStar = isShapeColor(STAR, RED);
+const isGreenSquare = isShapeColor(SQUARE, GREEN);
+const isWhiteTriangle = isShapeColor(TRIANGLE, WHITE);
+const isWhiteCircle = isShapeColor(CIRCLE, WHITE);
+const isBlueCircle = isShapeColor(CIRCLE, BLUE);
+const isOrangeSquare = isShapeColor(SQUARE, ORANGE);
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
 export const validateFieldN1 = allPass([
@@ -52,17 +58,17 @@ export const validateFieldN1 = allPass([
 export const validateFieldN2 = compose(
   gte(__, 2),
   length,
-  filter(isColor("green")),
-  ({ star, square, triangle, circle }) => [star, square, triangle, circle]
+  filter(isColor(GREEN)),
+  shapesToArray
 );
 
 // 3. Количество красных фигур равно кол-ву синих.
 export const validateFieldN3 = compose(
-  (shapes) => length(filter(isRed, shapes)) === length(filter(isBlue, shapes)),
+  converge(equals, [countShapesByColor(RED), countShapesByColor(BLUE)]),
   shapesToArray
 );
 
-// 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
+// 4. Синий круг, красная звезда, оранжевый квадрат, треугольник любого цвета.
 export const validateFieldN4 = allPass([
   isBlueCircle,
   isRedStar,
@@ -71,32 +77,35 @@ export const validateFieldN4 = allPass([
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
 export const validateFieldN5 = compose(
-    anyPass([gte(__, 3), equals(4)]),
-  values,
-  countNonWhiteColors,
+  hasAtLeastThreeSameColor,
+  sortColors,
+  shapesWithoutWhite,
   shapesToArray
 );
 
-// 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = ({ star, square, triangle, circle }) =>
+// 6. Ровно две зеленые фигуры (одна из них треугольник), плюс одна красная. Четвертая любая.
+export const validateFieldN6 = compose(
   allPass([
-    compose(equals(2), length, filter(isGreen), values),
-    compose(equals(1), length, filter(isRed), values),
-    compose(isGreen, getColor("triangle")),
-  ])({ star, square, triangle, circle });
+    compose(equals(2), length, filter(isColor(GREEN)), values),
+    compose(equals(1), length, filter(isColor(RED)), values),
+    isShapeColor(TRIANGLE, GREEN),
+  ]),
+  ({ star, square, triangle, circle }) => ({ star, square, triangle, circle })
+);
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = compose(all(isColor("orange")), shapesToArray);
+export const validateFieldN7 = compose(all(isColor(ORANGE)), shapesToArray);
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
 export const validateFieldN8 = compose(
-  (color) => !isRed(color) && !isWhite(color),
-  getColor("star")
+  converge(allPass, map(isColor, [RED, WHITE])),
+  getColor(STAR),
+  not
 );
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = compose(all(isColor("green")), shapesToArray);
+export const validateFieldN9 = compose(all(isColor(GREEN)), shapesToArray);
 
-// 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
+// 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета.
 export const validateFieldN10 = ({ triangle, square }) =>
-  triangle === square && !isColor("white")(triangle);
+  equals(triangle, square) && !isColor(WHITE)(triangle);
